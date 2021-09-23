a0903cc7d97392b858f99b3a0fff340b190b5f71
crapboy <giulianopertile@gmail.com>
2021-09-22 21:26:49 -0300

    se elimino todo lo viejo y lo de R

--- Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Section 36 - Convolutional Neural Networks (CNN) Nuevo/Python/convolutional_neural_network_comentado.py
+++ Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/Section 36 - Convolutional Neural Networks (CNN) Nuevo/Python/convolutional_neural_network_comentado.py
@@ -5,7 +5,8 @@
 
 # Part 1 - Building the CNN
 
-#Importing the Keras libraries and packages
+# Importing the Keras libraries and packages
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -16,59 +17,60 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-#32 features detector de 3x3 cada cuadrado
+# 32 features detector de 3x3 cada cuadrado
 # input_shape 64x64 es el tamaño de la imagen y el 3 es para que mantegan los colres
 # 2 es blanco y negro
 # relu es para para tener no linearidad
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
 # Step 2 - Pooling
-#reducimos el tamaño del feature map
-##los argumentos es el tamaño del cuadrado
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+# reducimos el tamaño del feature map
+# los argumentos es el tamaño del cuadrado
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Adding a second convolutional layer
-#esto para evitar el overfitting y mas accuracy
+# esto para evitar el overfitting y mas accuracy
 # sacamos input_shape porque ya tenemos algo antes
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(
+    optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 
-from keras.preprocessing.image import ImageDataGenerator
 
-#aca le aplicamos augmenttion, osea a las imaganes,
-#las movemos, le hacemos zoom etc para hacerlas mas random
+# aca le aplicamos augmenttion, osea a las imaganes,
+# las movemos, le hacemos zoom etc para hacerlas mas random
 # y aumentar la precision
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64), #dimensiones de las imagenes
-                                                 batch_size = 32,
-                                                 class_mode = 'binary') #porque tenes dos categorias gatos o perros
+                                                 # dimensiones de las imagenes
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')  # porque tenes dos categorias gatos o perros
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000, #cantidad de imaganes en cada epoch
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000) #cantidad en test set
+                         steps_per_epoch=8000,  # cantidad de imaganes en cada epoch
+                         epochs=25,
+                         validation_data=test_set,
+                         validation_steps=200)  # cantidad en test set
